fba4a0d82bbbde7a1182804d0af75d8e0e8b2153
Andrei2348 <makarevichandco@gmail.com>
2023-12-13 15:18:07 +0300

    Изменения

--- actions/low_priority.py
+++ actions/low_priority.py
@@ -1,5 +1,4 @@
 from .actions import summa, minus
-from . import logging
 
 def get_result_low_priority(first_elem, elems, actions, count):
    if count < (len(actions) + 1):
@@ -12,5 +11,4 @@ def get_result_low_priority(first_elem, elems, actions, count):
             first_elem = minus(first_elem, elems[count])
 
       first_elem = get_result_low_priority(first_elem, elems, actions, count + 1)
-   logging.write_log('Производим действия вычитания и сложения', 0)
    return first_elem
